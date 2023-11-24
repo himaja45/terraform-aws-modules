aa908df41e74881acfd13384edd2ef904bf883ba
Himaja <himaja1999@gmail.com>
2023-11-23 20:04:00 -0500

    final commits

--- simple/main.tf
+++ simple/main.tf
@@ -1,6 +1,9 @@
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-east-1"  # replace with your desired AWS region
+  access_key = "AKIAZ27LHRMD4BPPEXHI"
+  secret_key = "TUvht4kruL5icSyNe0/cRDvROizKarQixv7c3S/B"
 }
+
 
 resource "aws_instance" "example" {
   ami           = "ami-0230bd60aa48260c6"
