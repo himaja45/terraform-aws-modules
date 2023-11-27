cfae8e4dc0f2a58a8c9c23512ff87be3d1abaf6a
himaja45 <132203516+himaja45@users.noreply.github.com>
2023-11-26 19:28:24 -0500

    Update backend.tf

--- simple/backend.tf
+++ simple/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
     organization = "Terraform_devops_demo"
-    workspace = "terra_demo"
+    workspace = "https://app.terraform.io/app/Terraform_devops_demo/workspaces/terra_demo"
   }
 }
