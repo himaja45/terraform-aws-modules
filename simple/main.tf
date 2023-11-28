b2989d58a3d2eeaa1431890d4479a91614682040
himaja45 <132203516+himaja45@users.noreply.github.com>
2023-11-27 20:23:51 -0500

    Update main.tf

--- simple/main.tf
+++ simple/main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   region     = "us-east-1"  
-  export AWS_ACCESS_KEY_ID="AWS_ACCESS_KEY"
-  export AWS_SECRET_ACCESS_KEY="AWS_SECRET_KEY"
-  export AWS_REGION="us-east-1"
+  export AWS_ACCESS_KEY_ID = "AWS_ACCESS_KEY"
+  export AWS_SECRET_ACCESS_KEY = "AWS_SECRET_KEY"
+  export AWS_REGION = "us-east-1"
 }
 
 
