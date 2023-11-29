822df012907e4a67fc083d928b3417e33c5c5182
himaja45 <132203516+himaja45@users.noreply.github.com>
2023-11-28 21:57:36 -0500

    Update vpc.tf

--- simple/vpc.tf
+++ simple/vpc.tf
@@ -2,7 +2,7 @@ provider "vpc" {
   region = "us-east-1"
 }
 
-module "aws_modules" "example" {
+module "aws_modules" {
   source = "./terraform-aws-files"
   ami = "ami-0230bd60aa48260c6"
   instance_type = "t2.micro"
