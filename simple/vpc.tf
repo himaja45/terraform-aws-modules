f08a5c3131f3b2c8289a8d96d06b3c1434aeb7df
himaja45 <132203516+himaja45@users.noreply.github.com>
2023-11-28 21:53:09 -0500

    Create vpc.tf

---
+++ simple/vpc.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+modules "aws_modules" "example" {
+  source = "./terraform-aws-files"
+  ami = "ami-0230bd60aa48260c6"
+  instance_type = "t2.micro"
+  key_name = "terraform_modules"
+  tags = {
+    name = "modules-example"
+  }
+}
