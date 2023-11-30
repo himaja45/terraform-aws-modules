ff903d97aae856de8c3aacd96d7eb52ad288b699
himaja45 <132203516+himaja45@users.noreply.github.com>
2023-11-30 17:42:18 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -11,11 +11,37 @@ variable "project_name" {
 variable "environment" {}
 
 # vpc variables
-variable "vpc_cidr" {}
-variable "public_subnet_az1_cidr" {}
-variable "private_app_subnet_az1_cidr" {}
-variable "private_app_subnet_az2_cidr" {}
-variable "private_app_subnet_az3_cidr" {}
-variable "private_data_subnet_az1_cidr" {}
-variable "private_data_subnet_az2_cidr" {}
-variable "private_data_subnet_az3_cidr" {}
+variable "vpc_cidr" {
+    type = string
+    description = "value of cidr range for vpc"
+    default = "10.0.0.0/16"
+}
+variable "public_subnet_az1_cidr" {
+    type = string
+    description = "value of public subnet for vpc"
+    default = "10.0.1.0/24"
+}
+variable "private_app_subnet_az1_cidr" {
+    type = string
+    description = "value of private subnet for vpc"
+}
+variable "private_app_subnet_az2_cidr" {
+    type =  string
+     description = "value of private subnet"
+}
+variable "private_app_subnet_az3_cidr" {
+    type = string
+    desription = "value of private subnet"
+}
+variable "private_data_subnet_az1_cidr" {
+    type = string
+    description = "value of private data subnet"
+}
+variable "private_data_subnet_az2_cidr" {
+    type = string
+    description = "value of private data subnet"
+}
+variable "private_data_subnet_az3_cidr" {
+    type = string
+    description = "value of private data subnet"
+}
