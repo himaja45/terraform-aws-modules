0ae00ae8f5f5534017419c3a345959c483ed63e6
Himaja <himaja1999@gmail.com>
2023-12-01 20:50:19 -0500

    updated variables

--- simple/vpc.tf
+++ simple/vpc.tf
@@ -1,13 +1,13 @@
 module "servers" {
   source = "./.."
-  var.vpc_cidr
-  var.public_subnet_az1_cidr
-  var.private_app_subnet_az1_cidr
-  var.private_app_subnet_az2_cidr
-  var.private_app_subnet_az3_cidr
-  var.private_data_subnet_az1_cidr
-  var.private_data_subnet_az2_cidr
-  var.private_data_subnet_az3_cidr
+  var.vpc_cidr = 10.0.0.0/16
+  var.public_subnet_az1_cidr = 10.0.101.0/24
+  var.private_app_subnet_az1_cidr = 10.0.1.0/24
+  var.private_app_subnet_az2_cidr = 10.0.2.0/24
+  var.private_app_subnet_az3_cidr = 10.0.3.0/24
+  var.private_data_subnet_az1_cidr = 10.0.21.0/24
+  var.private_data_subnet_az2_cidr = 10.0.22.0/24
+  var.private_data_subnet_az3_cidr = 10.0.23.0/24
 }
 
 terraform {
